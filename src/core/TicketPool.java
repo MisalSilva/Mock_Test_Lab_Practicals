408c8e95e3167bd683ff9e813b64ddf332692ac0
MisalSilva <misal.20230183@iit.ac.lk>
2025-01-08 01:09:15 +0530

    Update: Update the TicketPool

--- src/core/TicketPool.java
+++ src/core/TicketPool.java
@@ -1,4 +1,5 @@
 package core;
+import logging.Logger;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -8,10 +9,20 @@ public class TicketPool implements TicketOperation {
     @Override
     public synchronized void addTickets(String ticket) {
         tickets.add(ticket);
+        Logger.logTicketOperation("add", ticket);
     }
     @Override
     public synchronized String removeTicket() {
         return tickets.isEmpty() ? null : tickets.remove(0);
+        if (!tickets.isEmpty()){
+            String ticket=tickets.remove(0);
+            Logger.logTicketOperation(ticket,"remove");
+            return ticket;
+        }
+        else{
+            Logger.log("No tickets available to remove");
+            return null;
+        }
     }
     public int getTicketCount() {
         return tickets.size();
